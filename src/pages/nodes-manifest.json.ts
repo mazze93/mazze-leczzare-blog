b8b0fac350c6054fd496af05c1d9585ae612ae40
Mazze LeCzzare Frazer <mazze@mazzeleczzare.com>
2026-07-04 19:40:30 -0400

    Refactor GET route to use collectAllNodesrefactor: use collectAllNodes in manifest endpoint
    Signed-off-by: Mazze LeCzzare Frazer <mazze@mazzeleczzare.com>

--- src/pages/nodes-manifest.json.ts
+++ src/pages/nodes-manifest.json.ts
@@ -1,23 +1,13 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
-import { aggregateNodes, type RawEntry } from "../utils/nodes";
+import { collectAllNodes } from "../utils/collectNodes";
 
 export const GET: APIRoute = async () => {
-  const blog = await getCollection("blog", ({ data }) => !data.draft);
-  const signal = await getCollection("signal", ({ data }) => !data.draft);
-
-  const entries: RawEntry[] = [
-    ...blog.map((e) => ({ id: e.id, type: "blog" as const, data: e.data })),
-    ...signal.map((e) => ({ id: e.id, type: "signal" as const, data: e.data })),
-  ];
-
-  const nodes = aggregateNodes(entries);
+  const nodes = await collectAllNodes();
   const generatedAt = nodes.length > 0 ? nodes[0].lastTouched : null;
   const body = JSON.stringify({
     generatedAt,
     nodes,
   });
-
   return new Response(body, {
     headers: { "Content-Type": "application/json; charset=utf-8" },
   });
